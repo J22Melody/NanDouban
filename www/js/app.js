77f74dc1fc9b12df7a735f5845ae0fd7952eaef3
Chenbo Li <lichenbo1949@gmail.com>
2014-06-24 19:47:37 +0800

    new branch

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,5 @@
-// create module
+    // create module
+    // @Annotation: module `app' depends on the modules in the array.
 angular.module('app', 
   ['ionic','app.config','app.controllers','app.directives','app.filters','app.services']);
 
